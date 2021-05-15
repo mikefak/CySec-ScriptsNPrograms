a0e596694806db60f218789808c5021241794a5b
Michael Fakhouri <75512760+mffakhouri@users.noreply.github.com>
2021-05-15 12:15:24 -0500

    Passpol v1.0
    Added complexity and faillock options.

--- Passpol.sh
+++ Passpol.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-#Passpol setup by mikeffakhouri and youngysf
+#Password Policy Script by mikeffakhouri and youngysf
 
+#Script made to easily establish basic and 
+
+
+#WARNING: The following script will edit pre-existing password policy configurations, proceed with caution
+
+pwq=$(apt-get install libpam-pwquality 2>/dev/null)
+
+if [ "$pwq" ];
+then
+        apt-get install libpam-pwquality
+else
+    echo -e "Error: make sure the script is ran as an administrator or that you have a stable internet connection"
+    exit 1
+fi
 
 function complex()
 {
-    #update libpam-pwquality and ensure sudo access 
-    pwq=$(apt-get install libpam-pwquality 2>/dev/null)
-
-    if [ "$pwq" ];
-    then
-        apt-get install libpam-pwquality
-    else
-        echo -e "Error: make sure the script is ran as an administrator or that you have a stable internet connection"
-        exit 1
-    fi
 
    #Preset and reccomended complexity for passwords (>=1 uppercase, lowercase, digit, and special character)
     upper="ucredit=-1"
@@ -22,19 +26,45 @@ function complex()
     digit="dcredit=-1"
     special="ocredit=-1"
     
-    echo -e $upper $lower $digit $special will be appended to enforce pw complexity
-
-    echo -e "$upper\n$lower\n$digit\n$special" | tee -a /etc/security/pwquality.conf >/dev/null
+    echo -e "\n$upper\n$lower\n$digit\n$special\n" | tee -a /etc/security/pwquality.conf
+    echo "The above libpam-pw complexity rules have been appended to /etc/security/pwquality.conf"
  
-    #insert actual implementation
+}
+
+function minlen()
+{
+
+    #Default min password length to eight
+    minln="minlen=8"
+
+    echo -e "$minln\n" | tee -a /etc/security/pwquality.conf
+
+    echo -e "The above libpam-pw minimum password length rules have been appended to /etc/security/pwquality.conf"
+}
+
+function faillock()
+{
+
+    #Defaults to 5 attempts (15 min interval from first failure) and 15 minute lockout period
+    deny="deny=5"
+    failint="fail_interval=900"
+    utime="unlock_time=600"
+
+    echo -e "\n$deny\n$failint\n$utime" | tee -a /etc/security/faillock.conf
+
+    echo -e "The above faillock configurations have been appended to /etc/security/faillock.conf"
+
+
 }
 
 function optlist()
 {
 
     echo -e "How to use: ./Passpol.sh (option)\n"
-    echo -e "Opt List:\n"
-    echo -e "-c     Installs libpam-pwquality and enforces Password Compexity --> /etc/security/pwquality.conf"
+    echo -e "Opt List:\t \"-->\" symbolizes the configuration file that will be altered\n"
+    echo -e "-c     Appends password complexity rules (lowerchar, upperchar, specialchar, digit) --> /etc/security/pwquality.conf\n"
+    echo -e "-l     Establishes Password Minimum Length --> /etc/security/pwquality.conf\n"
+    echo -e "-k     Lockouts user for (x) seconds after failing (x) password attempts --> /etc/security/faillock.conf"
 
 }
 OPTIONS="$1"
@@ -50,7 +80,23 @@ function start() {
 
         if [ "$OPTIONS" == "-c" ] || [ "$OPTIONS" == "c" ];
         then
-        complex
+            complex
+
+        else
+
+            if [ "$OPTIONS" == "-l" ] || [ "$OPTIONS" == "l" ];
+            then
+                minlen
+            
+            else
+
+                if [ "$OPTIONS" == "-k" ] || [ "$OPTIONS" == "k" ];
+                then
+                    faillock
+                
+                fi
+            
+            fi
 
         fi 
     fi
